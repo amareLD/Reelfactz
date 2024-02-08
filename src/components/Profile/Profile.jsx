5199f3843a62bdb2b92217d38c1e358b979b8aa3
amareLD <80774058+amareLD@users.noreply.github.com>
2024-02-08 14:32:57 +0530

    create-login-function

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
+
+// import { RatedCards } from '..';
+
+import { userSelector } from '../../features/auth';
+// import { useGetUsersListQuery } from '../../services/TMDB';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { user } = useSelector(userSelector);
+
+  const favoriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  console.log(user);
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies?.results?.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>Favorite Movies</Box>
+      )}
+    </Box>
+  );
 }
