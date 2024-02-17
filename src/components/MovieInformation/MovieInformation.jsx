54fba8a6cabd5ff859f521ad73d7d9904c26ae95
amareLD <80774058+amareLD@users.noreply.github.com>
2024-02-17 23:27:48 +0530

    movie_information-1

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -23,7 +23,9 @@ import { Link, useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
+import { selectGenreOrCategory, genreOrCategory } from '../../features/currentGenreOrCategory';
 import useStyles from './styles';
+import genreCategoryIcons from '../../assets/genres';
 
 import { useGetMovieQuery } from '../../services/TMDB';
 
@@ -32,6 +34,9 @@ export default function MovieInformation() {
   const { data, error, isFetching } = useGetMovieQuery(id);
   const classes = useStyles();
   const noImage = 'https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg';
+  const ratings = data?.vote_average;
+  const ratingInt = Number(ratings).toFixed(1);
+  const dispatch = useDispatch();
 
   if (isFetching) {
     return (
@@ -47,6 +52,9 @@ export default function MovieInformation() {
       </Box>
     );
   }
+
+  console.log(data);
+
   return (
     <Grid container className={classes.containerSpaceAround}>
       <Grid item sm={12} lg={4}>
@@ -60,9 +68,50 @@ export default function MovieInformation() {
           alt={data?.title}
         />
       </Grid>
-      <Grid item container>
-        <Grid item cdirection="column" lg={7}>
-          <Typography variant="h3" align="center" gutterBottom> {data?.title}</Typography>
+
+      <Grid item container direction="column" lg={7}>
+        { /* title, tagline, rating, runtime, spoken languages */ }
+        <Typography variant="h3" align="center" gutterBottom>
+          {' '}
+          {data?.title}({data.release_date.split('_')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {' '}
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceAround}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              style={{ marginLeft: '10px' }}
+            >
+              {ratingInt} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime}min |{' '}
+            {data?.spoken_languages?.length > 0
+              ? data?.spoken_languages[0].name
+              : ''}
+          </Typography>
+        </Grid>
+        { /* genres and overview */ }
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link
+              key={genre?.name}
+              className={classes.links}
+              to="/"
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+            >
+              <img src={genreCategoryIcons[genre?.name.toLowerCase()]} className={classes.genreImage} height={30} />
+              <Typography color="textPrimary" variant="subtitle1">
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
         </Grid>
       </Grid>
     </Grid>
--- src/components/MovieInformation/styles.js
+++ src/components/MovieInformation/styles.js
@@ -16,16 +16,16 @@ export default makeStyles((theme) => ({
     boxShadow: '0.5em 1em 1em rgb(64, 64, 70)',
     width: '80%',
 
-    [theme.breakpoints.down('sm')]: {
-      margin: '0 auto',
-      width: '100%',
-      height: '350%',
-      marginBottom: '30px',
-    },
     [theme.breakpoints.down('md')]: {
       margin: '0 auto',
       width: '50%',
       height: '350px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      margin: '0 auto',
+      width: '100%',
+      height: '350PX',
+      marginBottom: '30px',
     },
   },
 
